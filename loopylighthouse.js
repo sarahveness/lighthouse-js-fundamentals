0b8f49abc93a1b05be259b68920f958ee893b31e
Sarah Veness <sarahhveness@gmail.com>
2016-08-16 06:16:27 +0000

    LoopyLighthouse exercise

---
+++ loopylighthouse.js
@@ -0,0 +1,16 @@
+var i = 100;
+
+for (; i <= 200; i++)
+ {
+  if (i % 3 === 0 & i % 4 === 0)
+    console.log("LoopyLighthouse");
+
+  else if (i % 3 === 0)
+    console.log("Loopy");
+
+  else if (i % 4 === 0)
+    console.log("Lighthouse");
+
+  else
+    console.log(i);
+}
